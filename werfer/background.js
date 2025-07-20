db8dcf30a99d413805ccf9d68d8ef96f9dd97bc7
stormofice <58337328+stormofice@users.noreply.github.com>
2025-07-20 19:02:34 +0200

    Basic link saving functionality

--- werfer/background.js
+++ werfer/background.js
@@ -1,8 +1,17 @@
-
 browser.runtime.onInstalled.addListener(() => {
     console.log("Werfer loaded");
 });
 
-browser.pageAction.onClicked.addListener(() => {
-    console.log("Click");
+browser.pageAction.onClicked.addListener(async (currentTab) => {
+    const options = await browser.storage.sync.get();
+
+    const resp = await fetch(`${options.backend_url}/faenge/save`, {
+        method: "POST",
+        body: JSON.stringify({
+            title: currentTab.title,
+            url: currentTab.url,
+        }),
+        headers: {"Content-Type": "application/json", "X-Api-Key": options.api_key},
+    });
+    console.log("got response", resp);
 });
---
+++ werfer/options.js
@@ -0,0 +1,16 @@
+async function saveOptions(e) {
+    e.preventDefault();
+    await browser.storage.sync.set({
+        backend_url: document.querySelector("#backend_url").value,
+        api_key: document.querySelector("#api_key").value,
+    });
+}
+
+async function restoreOptions() {
+    const options = await browser.storage.sync.get();
+    document.querySelector("#backend_url").value = options.backend_url;
+    document.querySelector("#api_key").value = options.api_key;
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
